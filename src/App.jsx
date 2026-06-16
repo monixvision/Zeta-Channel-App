d5bddb08d42d8070d49ebbd61454bd9ffb52cbb5
monixvision <monixvision@gmail.com>
2026-06-16 21:24:09 +0200

    añadido los router y browser route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { BrowserRouter, Routes, Route, /* Navigate */ } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 /* import Footer from './components/Footer'; */
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
     <main>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </main>
     {/* <Footer /> */}
-  </>
+  </BrowserRouter>
 )
 export default App
