99c93930ab9059bca0dae5b8014d28bd5e93ec1f
monixvision <monixvision@gmail.com>
2026-06-19 18:51:35 +0200

    lo he puesto la mayoria en comentarios para ya luego ir poco a poco añadiendolos pq sino me daba error

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route, /* Navigate */ } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Tickets from './pages/Tickets.jsx';
+/* import Tickets from './pages/Tickets.jsx';
 import Cartelera from './pages/Cartelera';
 import Festival from './pages/Festival';
 import Agenda from './pages/Agenda';
-import User from './pages/User';
+import User from './pages/User'; */
 import ScrollToTop from './components/routes/ScrollToTop';
-/* import Footer from './components/Footer'; */
+/* import Error404 from './pages/Error404';
+import Articulo from './pages/Articulo';
+import Footer from './components/Footer'; */
 
 const App = () => (
   <BrowserRouter>
@@ -18,13 +20,19 @@ const App = () => (
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Tickets" element={<Tickets />} />
+        {/*   <Route path="/Tickets" element={<Tickets />} />
         <Route path="/Cartelera" element={<Cartelera />} />
         <Route path="/Festival" element={<Festival />} />
         <Route path="/Agenda" element={<Agenda />} />
         <Route path="/User" element={<User />} />
+        <Route path="/Articulo" element={<Articulo />} />
+        <Route path="/Error404" element={<Error404 />} />
+        <Route path="*" element={<Navigate to="/Error404" replace />} /> */}
+        {/* Ruta para cualquier otra ruta no definida y usamos Navigate para redirigir a la página de error */}
       </Routes>
     </main>
+
+    {/* <Footer /> */}
 
   </BrowserRouter>
 )
