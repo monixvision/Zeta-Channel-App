24e36dc3b7eaa68307ee441075119dc580aee1c2
monixvision <monixvision@gmail.com>
2026-06-21 21:05:28 +0200

    empezamos a incorporar los props de la base de datos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,31 @@
+import { /* Link */ } from 'react-router-dom';
+
 import btnMas from '../assets/img/icon/icon-btn-mas.svg';
+
+const Card = ({ card }) => {
+    const { tituloEsp, imagen, tipo, duracion, /* dia, horario, slug */ } = card;
+
+    return (
+        <div>
+            <ul>
+                <li>
+                    <h1>{tituloEsp}</h1>
+                </li>
+                <li>
+                    <p>{tipo}</p>
+                    <p>{duracion}</p>
+                </li>
+                <li>
+                    {/* crear componente de btn general */}
+                </li>
+            </ul>
+            <div>
+                <img src={imagen} alt="Portada" />
+                <img src={btnMas} alt="Más información" />
+            </div>
+        </div>
+    )
+
+}
+
+export default Card;
