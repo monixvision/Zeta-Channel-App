44bb1d1b1e18f4301f77592f494a6f6d85755ccc
monixvision <monixvision@gmail.com>
2026-06-21 18:28:46 +0200

    el pop up ya tiene todo listo incluso el js donde la x es el btn de salida

--- src/components/PopupMenu.jsx
+++ src/components/PopupMenu.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import reSize from "../assets/img/icon/icon-resize.svg";
 import minimizar from "../assets/img/icon/icon-minimizar.svg";
 import salir from "../assets/img/icon/icon-salir.svg";
@@ -9,23 +11,64 @@ import agendaIcon from '../assets/img/icon/icon-agenda.svg';
 import userIcon from '../assets/img/icon/icon-user.svg';
 import festivalIcon from '../assets/img/icon/icon-festival.svg';
 
-const PopupMenu = () => (
-    <nav className="rounded-xl bg-pink mt-[60px]">
-        <ul className="flex gap-6 justify-end rounded-t-xl bg-dark py-3 px-4">
-            <li><img src={minimizar} alt="Minimizar" className="w-4 h-4" /></li>
-            <li><img src={reSize} alt="ReSize" className="w-4 h-4" /></li>
-            <li><img src={salir} alt="Saliz" className="w-4 h-4" /></li>
-        </ul>
-        <ul className="flex flex-col gap-6 p-6 text-2xl font-pixel">
-            <li><Link to="/"><img src={homeIcon} alt="Home" className="invert dark:invert-0 w-6 h-6" />Home</Link></li>
-            <li><Link to="/Tickets">Tickets</Link></li>
-            <li><Link to="/Cartelera">Cartelera</Link></li>
-            <li><Link to="/Festival">Festival</Link></li>
-            <li><Link to="/Agenda">Agenda</Link></li>
-            <li><Link to="/User">User</Link></li>
-        </ul>
-    </nav>
-)
+/* intentar que puedas arrastrarlo por la pantalla */
+
+const PopupMenu = () => {
+
+    const [popupIsOpen, setpopupIsOpen] = useState(false);
+
+    return (
+        <nav className={`flex flex-col rounded-xl bg-pink absolute left-0 right-0 top-0 bottom-0 my-auto h-fit mx-auto w-fit ${popupIsOpen ? '' : 'hidden'} `} >
+            {/* el w-fit es lo que me ha dejado ponerlo en el centro pq se extendía */}
+            < ul className="flex gap-6 justify-end rounded-t-xl bg-dark py-3 px-4" >
+                <li><img src={minimizar} alt="Minimizar" className="w-4 h-4" /></li>
+                <li><img src={reSize} alt="ReSize" className="w-4 h-4" /></li>
+                <li>
+                    <button onClick={() => setpopupIsOpen(false)} className="cursor-pointer">
+                        <img src={salir} alt="Salir" className="w-4 h-4" />
+                    </button>
+                </li>
+            </ul >
+            <ul className="flex flex-col gap-6 p-6 text-2xl font-pixel px-15">
+                <li>
+                    <Link to="/" className="flex gap-6">
+                        <img src={homeIcon} alt="Home" className="w-6 h-6" />
+                        <span className="uppercase">Home</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Tickets" className="flex gap-6">
+                        <img src={ticketIcon} alt="Tickets" className="w-6 h-6" />
+                        <span className="uppercase">Tickets</span>
+                    </Link></li>
+                <li>
+                    <Link to="/Cartelera" className="flex gap-6">
+                        <img src={carteleraIcon} alt="Cartelera" className="w-6 h-6" />
+                        <span className="uppercase">Cartelera</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Festival" className="flex gap-6">
+                        <img src={festivalIcon} alt="Festival" className="w-6 h-6" />
+                        <span className="uppercase">Festival</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Agenda" className="flex gap-6">
+                        <img src={agendaIcon} alt="Agenda" className="w-6 h-6" />
+                        <span className="uppercase">Agenda</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/User" className="flex gap-6">
+                        <img src={userIcon} alt="User" className="w-6 h-6" />
+                        <span className="uppercase">User</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav >
+    )
+}
 
 export default PopupMenu;
 
