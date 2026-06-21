f1b4e46d8a7235bba3e88074ebfd3e50478a7420
monixvision <monixvision@gmail.com>
2026-06-21 16:32:31 +0200

    he creado lo basic de cada pagina para que no me de error

---
+++ src/pages/Agenda.jsx
@@ -0,0 +1,10 @@
+
+const Agenda = () => (
+
+    <div>
+        <h1>AGENDA</h1>
+    </div>
+
+);
+
+export default Agenda;
---
+++ src/pages/Cartelera.jsx
@@ -0,0 +1,9 @@
+const Cartelera = () => (
+
+    <div>
+        <h1>CARTELERA</h1>
+    </div>
+
+);
+
+export default Cartelera;
