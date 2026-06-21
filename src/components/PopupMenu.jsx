6befbb77e8b3f6f56a778f98387acdb3373e464b
monixvision <monixvision@gmail.com>
2026-06-21 17:19:01 +0200

    estamos montando el popup del menú

--- src/components/PopupMenu.jsx
+++ src/components/PopupMenu.jsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
+import reSize from "../assets/img/icon/icon-resize.svg";
+import minimizar from "../assets/img/icon/icon-minimizar.svg";
+import salir from "../assets/img/icon/icon-salir.svg";
+import ticketIcon from '../assets/img/icon/icon-ticket.svg';
+import homeIcon from '../assets/img/icon/icon-home.svg';
+import carteleraIcon from '../assets/img/icon/icon-cartelera.svg';
+import agendaIcon from '../assets/img/icon/icon-agenda.svg';
+import userIcon from '../assets/img/icon/icon-user.svg';
+import festivalIcon from '../assets/img/icon/icon-festival.svg';
 
 const PopupMenu = () => (
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
+    <nav className="rounded-xl bg-pink mt-[60px]">
+        <ul className="flex gap-6 justify-end rounded-t-xl bg-dark py-3 px-4">
+            <li><img src={minimizar} alt="Minimizar" className="w-4 h-4" /></li>
+            <li><img src={reSize} alt="ReSize" className="w-4 h-4" /></li>
+            <li><img src={salir} alt="Saliz" className="w-4 h-4" /></li>
+        </ul>
+        <ul className="flex flex-col gap-6 p-6 text-2xl font-pixel">
+            <li><Link to="/"><img src={homeIcon} alt="Home" className="invert dark:invert-0 w-6 h-6" />Home</Link></li>
             <li><Link to="/Tickets">Tickets</Link></li>
             <li><Link to="/Cartelera">Cartelera</Link></li>
             <li><Link to="/Festival">Festival</Link></li>
