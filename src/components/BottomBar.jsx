1a29353036a07272c53e8c79198a2ee464a45524
monixvision <monixvision@gmail.com>
2026-06-21 15:51:55 +0200

    ya está el diseño planetado para empezar con la parte de js de cambio de pagina con la linea inferior

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import ticketIcon from '../assets/img/icon/icon-ticket.svg';
 import homeIcon from '../assets/img/icon/icon-home.svg';
 import carteleraIcon from '../assets/img/icon/icon-cartelera.svg';
-import festivalIcon from '../assets/img/icon/icon-festival.svg';
 import agendaIcon from '../assets/img/icon/icon-agenda.svg';
 import userIcon from '../assets/img/icon/icon-user.svg';
 
@@ -12,13 +11,38 @@ const BottomBar = () => (
     /* hacer que el pop up se pueda mover */
     <div className="flex flex-col items-center justify-center fixed bottom-0 left-0 w-full bg-dark dark:bg-blue text-white dark:text-black z-9999">
         <nav>
-            <ul>
-                <li><Link to="/"><img className="w-6 h-6" src={homeIcon} alt="Home" />HOME</Link></li>
-                <li><Link to="/tickets"><img src={ticketIcon} alt="Tickets" />TICKETS</Link></li>
-                <li><Link to="/cartelera"><img src={carteleraIcon} alt="Cartelera" />CARTELERA</Link></li>
-                <li><Link to="/festival"><img src={festivalIcon} alt="Festival" />FESTIVAL</Link></li>
-                <li><Link to="/agenda"><img src={agendaIcon} alt="Agenda" />AGENDA</Link></li>
-                <li><Link to="/user"><img src={userIcon} alt="User" />USER</Link></li>
+            <ul className="flex justify-around items-center w-full gap-3 py-3 font-pixel text-xs">
+                <li className="order-3 w-[63px]">
+                    <Link to="/" className="flex flex-col items-center justify-center gap-1 ">
+                        <img src={homeIcon} alt="Home" className="invert dark:invert-0 w-6 h-6" />
+                        HOME
+                        {/* <div className="w-1 h-1 bg-white dark:bg-black rounded-full mt-1"></div> */}
+                    </Link>
+                </li>
+                <li className="order-1 w-[63px]">
+                    <Link to="/tickets" className="flex flex-col items-center justify-center gap-1">
+                        <img src={ticketIcon} alt="Tickets" className="invert dark:invert-0 w-6 h-6" />
+                        TICKETS
+                    </Link>
+                </li>
+                <li className="order-2 w-[63px]">
+                    <Link to="/cartelera" className="flex flex-col items-center justify-center gap-1">
+                        <img src={carteleraIcon} alt="Cartelera" className="invert dark:invert-0 w-6 h-6" />
+                        CARTELERA
+                    </Link>
+                </li>
+                <li className="order-4 w-[63px]">
+                    <Link to="/agenda" className="flex flex-col items-center justify-center gap-1">
+                        <img src={agendaIcon} alt="Agenda" className="invert dark:invert-0 w-6 h-6" />
+                        AGENDA
+                    </Link>
+                </li>
+                <li className="order-5 w-[63px]">
+                    <Link to="/user" className="flex flex-col items-center justify-center gap-1">
+                        <img src={userIcon} alt="User" className="invert dark:invert-0 w-6 h-6" />
+                        USER
+                    </Link>
+                </li>
             </ul>
         </nav>
     </div>
