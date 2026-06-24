908a875cc41dcc3faef9dcb81adfd591aac0cf4f
monixvision <monixvision@gmail.com>
2026-06-24 16:55:41 +0200

    estamos ya construyendo el articulo

--- src/pages/Articulo.jsx
+++ src/pages/Articulo.jsx
@@ -1,7 +1,11 @@
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams, /* Navigate, */ Link } from 'react-router-dom';
 
 import CATALOGO from '../assets/data/films.js';
 /* import Error404 from './Error404.jsx'; */
+import labelCard2 from '../assets/img/label-card-vertical.svg';
+import Btn from '../components/Btn.jsx';
+import btnMas from '../assets/img/icon/icon-btn-mas.svg';
+
 
 const Articulo = () => {
     /* 1. Capturamos el slug de la URL */
@@ -18,10 +22,22 @@ const Articulo = () => {
     /* 4.Si la pelicula existe, mostramos su información */
 
     return (
-        <div className="flex flex-col gap-6 p-6">
-            <h1 className="text-4xl font-bold">{tituloEsp}</h1>
+        <div>
+            <img src={labelCard2} alt="Label Card" className="w-full absolute -top-4 left-4" />
+            <div className="flex flex-col gap-8 px-6 pt-14 pb-6 bg-blue rounded-4xl">
+                <div>
+                    <img src={imagen} alt={tituloEsp} className="w-full rounded-4xl" />
+                    <Btn to="/Entradas" text="comprar" variant='solidgreen' size='xs' font='sans' className="abolute bottom-0 top-0 left-0 right-0 mx-auto my-auto" />
+                    <Link to="/Entradas" className="absolute bottom-0 top-0 right-0 mx-auto my-auto w-full h-full">
+                        <img src={btnMas} alt="Más información" />
+                    </Link>
+                </div>
+                <h1 className="text-4xl font-bold">{tituloEsp}</h1>
+                <div>
 
-        </div>
+                </div>
+            </div >
+        </div >
     );
 }
 export default Articulo;
