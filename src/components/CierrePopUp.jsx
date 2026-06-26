8b7af4c0f53f502b5bd52ffeb0235d7d9238dd85
monixvision <monixvision@gmail.com>
2026-06-26 18:52:14 +0200

    he creado un componente para la parte de arriba de los pop up para ya tenerlo listo par aplicarlo donde sea

---
+++ src/components/CierrePopUp.jsx
@@ -0,0 +1,22 @@
+import reSize from "../assets/img/icon/icon-resize.svg";
+import minimizar from "../assets/img/icon/icon-minimizar.svg";
+import salir from "../assets/img/icon/icon-salir.svg";
+
+const CierrePopUp = ({ setpopupIsOpen }) => (
+
+    <ul className="flex gap-6 justify-end rounded-t-xl bg-dark py-3 px-5" >
+        <li className="flex items-center justify-center">
+            <img src={minimizar} alt="Minimizar" className="w-4 h-4 opacity-60" />
+        </li>
+        <li className="flex items-center justify-center">
+            <img src={reSize} alt="ReSize" className="w-4 h-4 opacity-60" />
+        </li>
+        <li className="flex items-center justify-center">
+            <button onClick={() => setpopupIsOpen(false)} className="cursor-pointer">
+                <img src={salir} alt="Salir" className="w-5 h-5" />
+            </button>
+        </li>
+    </ul >
+);
+
+export default CierrePopUp;
