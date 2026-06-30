d6b8bab80c1892edf1a85448bcd00d8a511d521c
monixvision <monixvision@gmail.com>
2026-06-30 21:00:29 +0200

    he quitado el margin que habia porqeu se lo he puesto al main

--- src/pages/Articulo.jsx
+++ src/pages/Articulo.jsx
@@ -2,11 +2,11 @@ import { useParams, /* Navigate, */ Link } from 'react-router-dom';
 
 import { CATALOGO } from '../data/films.js';
 /* import Error404 from './Error404.jsx'; */
-import labelCard2 from '../assets/img/label-card-vertical.svg';
 import Btn from '../components/Btn.jsx';
 import btnMas from '../assets/img/icon/icon-btn-mas.svg';
 import tiraRollText from '../assets/img/tira-roll-text.svg';
 import styles from './Articulo.module.css';
+import LabelCard from '../components/LabelCard.jsx';
 
 const Articulo = () => {
     /* 1. Capturamos el slug de la URL */
@@ -23,9 +23,14 @@ const Articulo = () => {
     /* 4.Si la pelicula existe, mostramos su información */
 
     return (
-        <div>
-            <img src={labelCard2} alt="Label Card" className="w-full absolute -top-4 left-4" />
-            <div className="flex flex-col gap-8 px-6 pt-14 pb-6 bg-blue rounded-4xl">
+        <div className="relative pt-8">
+            <div className="absolute bottom-full right-0 -mb-10 z-10 ml-[110px]">
+
+                <LabelCard className="w-full h-12 relative text-blue" />
+
+                {/* He tenido que crear un componente al svg para poder ponerle el hover como className y además hacer el group-hover para que se haga en modo grupo */}
+            </div>
+            <div className="flex flex-col gap-8 px-6 pt-14 pb-6 bg-blue rounded-l-4xl rounded-br-4xl">
                 <div>
                     <img src={imagen} alt={tituloEsp} className="w-full rounded-4xl" />
                     <Btn to="/Entradas" text="comprar" variant='solidgreen' size='xs' font='sans' className="abolute bottom-0 top-0 left-0 right-0 mx-auto my-auto" />
@@ -38,7 +43,7 @@ const Articulo = () => {
                     className={`w-full h-12 bg-repeat-x bg-cover ${styles.roll}`}>
                 </div>
                 <div>
-                    <div className="flex bg-balck justify-between">
+                    <div className="flex bg-black justify-between">
                         <h3 className="uppercase font-sans text-white text-base ">cartel</h3>
                         <div>
 
