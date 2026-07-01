c2200a2e940907e7588c75fc95a5ea8de4e5d2f8
monixvision <monixvision@gmail.com>
2026-07-01 20:07:23 +0200

    he conseguido que la imagen ocupe más ancho y he hecho que toda la card como tal sea clickable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ import LabelCard from './LabelCard.jsx';
 
 
 const Card = ({ card }) => {
-    const { tituloEsp, imagenCard, tipo, duracion, dia, horario, slug
+    const { tituloEsp, imagen, imagenCard, tipo, duracion, dia, horario, slug
     } = card;
 
     /* const [isHorario1Selected, setIsHorario1Selected] = useState(false);
@@ -15,7 +15,7 @@ const Card = ({ card }) => {
  */
 
     return (
-        <div className="relative mx-auto bg-pink hover:bg-blue rounded-l-3xl rounded-br-3xl flex flex-col group transition-colors p-4 duration-300">
+        <Link to={`/articulo/${slug}`} className="relative mx-auto bg-pink hover:bg-blue rounded-l-3xl rounded-br-3xl flex flex-col group transition-colors p-4 duration-300">
             <div className="absolute bottom-full right-0 -mb-[2px] z-10">
 
                 <LabelCard className="w-full relative text-pink group-hover:text-blue transition-colors duration-300" />
@@ -34,7 +34,7 @@ const Card = ({ card }) => {
                             <p className="text-[calc(1.25rem_+_0.25vw)] text-black  group-hover:text-white font-pixel">{duracion} min</p>
                         </li>
                         <li className="w-full h-[2px] bg-black group-hover:bg-white"></li>
-                        <li className="flex gap-2 md:justify-between">
+                        <li className="flex justify-between">
                             <div>
                                 <Btn text={`${dia} sep`} variant='solidblue' size='xs' font='pixel' />
                             </div>
@@ -50,7 +50,7 @@ const Card = ({ card }) => {
                                 </div>
                             </div>
                         </li>
-                        <li className="hidden flex justify-between">
+                        <li className="hidden flex gap-2 md:justify-between">
                             <div>
                                 <Btn text={`${dia} sep`} variant='solidblue' size='xs' font='pixel' />
                             </div>
@@ -69,9 +69,12 @@ const Card = ({ card }) => {
                     </ul>
 
                 </div>
-                <div>
+                <div className="flex-1/2">
                     <div className="h-full overflow-hidden rounded-3xl">
-                        <img src={imagenCard} alt={tituloEsp} className="will-change-transform w-full h-full object-cover" />
+                        <img src={imagenCard} alt={tituloEsp} className="will-change-transform w-full h-full object-cover md:hidden" />
+                        <div className="hidden md:block will-change-transform">
+                            <img src={imagen} alt={tituloEsp} className="  w-full h-full object-cover object-top" />
+                        </div>
 
                         {/* Cuando veias la tarjeta sin el will-change-transform la imagen se veia como sin renderizar o pixelada pero cuando acercaba con el zoom en el buscador como que lo arreglaba, le he preguntado a la ia para ver qué era y es que por defecto el buscador como que la renderiza desde el CPU. Y ahora saca la imagen del flujo normal de renderizado de la CPU y le asigna su propia capa de memoria dedicada en la tarjeta gráfica (GPU). Al ser la GPU la que ahora se encarga de pintar esa tarjeta, aplica algoritmos de suavizado mucho más potentes (como los que usa Photoshop o Figma), eliminando el pixelado o el aspecto borroso (ya no sé si eso afectará a la optimización) */}
 
@@ -82,7 +85,7 @@ const Card = ({ card }) => {
                     </Link>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 
 };
